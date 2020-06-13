964c14b05c031976fe2608af05f1716412a4e911
Mahmoud Mansour <mahmodlte@gmail.com>
2020-06-13 08:14:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+function findMatching(arr,str){
+    const found = arr.filter(element => {
+        console.log()
+      return element === str || element === str.toLowerCase()
+    })
+    return found
+}
+function fuzzyMatch(arr , str){
+      
+  const found  =  arr.filter(data => {
+    
+      if(data.substring(0, 2)=== str){
+         return data.toLowerCase().indexOf(str.toLowerCase()) !== -1
+      }
+      
+    
+ })
+ return found
+}
+function matchName(arr,str){
+  const match = arr.filter(driver => {
+    return driver.name.toLowerCase().indexOf(str.toLowerCase()) !== -1
+  });
+  return match
+  }
+  
